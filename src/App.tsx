15e97a62ae1c1fe74f8ef19948986118541c1d4c
georgesycabaccang <georgesycabaccang@gmail.com>
2024-01-10 00:44:55 +0800

    Feature: Add route guards

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import LandingPage from "./components/pages/LandingPage";
 import { Routes, Route } from "react-router-dom";
 import LoggedInPage from "./components/pages/LoggedInPage";
 import WeatherPage from "./components/pages/WeatherPage";
+import LoggedInRoutes from "./route-guards/LoggedInRoutes";
+import LoggedOutRoutes from "./route-guards/LoggedOutRoutes";
 
 function App() {
     return (
         <>
             <NavigationBar />
             <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/home" element={<LoggedInPage />} />
-                <Route path="/forecast" element={<WeatherPage />} />
+                <Route element={<LoggedOutRoutes />}>
+                    <Route path="/" element={<LandingPage />} />
+                </Route>
+                <Route element={<LoggedInRoutes />}>
+                    <Route path="/home" element={<LoggedInPage />} />
+                    <Route path="/forecast" element={<WeatherPage />} />
+                </Route>
             </Routes>
         </>
     );
