c1dc497db238ad1a1758e81d5932ea6a1bc02c52
georgesycabaccang <georgesycabaccang@gmail.com>
2024-01-10 00:08:56 +0800

    Fix: Fix warning <tr> cannot be a direct child of <table>

--- src/components/pages/WeatherPage.tsx
+++ src/components/pages/WeatherPage.tsx
@@ -11,100 +11,108 @@ export default function WeatherPage() {
     return (
         <div className="flex justify-center pt-[8rem] flex-col items-center gap-10">
             <table className="flex flex-col w-[75%] text-[1em] items-center shadow-lg">
-                {/* start of table header */}
-                <tr className="flex w-[100%] justify-center text-center font-bold ">
-                    <td className="w-[13%] border-t border-r border-l rounded-tl-md border-black">
-                        City
-                    </td>
-                    <td className="w-[13%] border-t border-r border-black">State</td>
-                    <td className="w-[11%] border-t border-r border-black">Country</td>
-                    <td className="w-[16%] border-t border-r border-black">Date (dd/mm/yyyy)</td>
-                    <td className="w-[11%] border-t border-r border-black">Temperature</td>
-                    <td className="w-[11%] border-t border-r border-black">Description</td>
-                    <td className="w-[8%] border-t border-r border-black">Main</td>
-                    <td className="w-[9%] border-t border-black">Pressure</td>
-                    <td className="w-[8%] border-t border-r border-l rounded-tr-md border-black">
-                        Humidity
-                    </td>
-                </tr>
-                {/* end of table header */}
+                <tbody className="w-[100%]">
+                    {/* start of table header */}
+                    <tr className="flex w-[100%] justify-center text-center font-bold ">
+                        <td className="w-[13%] border-t border-r border-l rounded-tl-md border-black">
+                            City
+                        </td>
+                        <td className="w-[13%] border-t border-r border-black">State</td>
+                        <td className="w-[11%] border-t border-r border-black">Country</td>
+                        <td className="w-[16%] border-t border-r border-black">
+                            Date (dd/mm/yyyy)
+                        </td>
+                        <td className="w-[11%] border-t border-r border-black">Temperature</td>
+                        <td className="w-[11%] border-t border-r border-black">Description</td>
+                        <td className="w-[8%] border-t border-r border-black">Main</td>
+                        <td className="w-[9%] border-t border-black">Pressure</td>
+                        <td className="w-[8%] border-t border-r border-l rounded-tr-md border-black">
+                            Humidity
+                        </td>
+                    </tr>
+                    {/* end of table header */}
 
-                {/* start of data */}
-                {forecasts.map((forecast, index) => {
-                    return (
-                        <tr
-                            key={index}
-                            className={`flex w-[100%] justify-center text-center ${
-                                index % 2 === 0 ? "bg-gray-200" : "bg-gray-50"
-                            }`}
-                        >
-                            <td
-                                className={`py-2 w-[13%] border-t border-r border-l border-black ${
-                                    forecasts.length - 1 === index ? "border-b rounded-bl-md" : ""
+                    {/* start of data */}
+                    {forecasts.map((forecast, index) => {
+                        return (
+                            <tr
+                                key={index}
+                                className={`flex w-[100%] justify-center text-center ${
+                                    index % 2 === 0 ? "bg-gray-200" : "bg-gray-50"
                                 }`}
                             >
-                                {forecast?.name}
-                            </td>
-                            <td
-                                className={`py-2 w-[13%] border-t border-r border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {forecast?.state}
-                            </td>
-                            <td
-                                className={`py-2 w-[11%] border-t border-r border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {forecast?.country}
-                            </td>
-                            <td
-                                className={`py-2 w-[16%] border-t border-r border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {timeConverter(forecast.forecastForTheDay!.date)}
-                            </td>
-                            <td
-                                className={`py-2 w-[11%] border-t border-r border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {forecast.forecastForTheDay?.temp}
-                            </td>
-                            <td
-                                className={`py-2 w-[11%] border-t border-r border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {forecast.forecastForTheDay?.description}
-                            </td>
-                            <td
-                                className={`py-2 w-[8%] border-t border-r border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {forecast.forecastForTheDay?.main}
-                            </td>
-                            <td
-                                className={`py-2 w-[9%] border-t border-black ${
-                                    forecasts.length - 1 === index ? "border-b" : ""
-                                }`}
-                            >
-                                {forecast.forecastForTheDay?.pressure}
-                            </td>
-                            <td
-                                className={`py-2 w-[8%] border-t border-r border-l  border-black ${
-                                    forecasts.length - 1 === index ? "border-b rounded-br-md" : ""
-                                }`}
-                            >
-                                {forecast.forecastForTheDay?.humidity}
-                            </td>
-                        </tr>
-                    );
-                })}
-                {/* end of data */}
+                                <td
+                                    className={`py-2 w-[13%] border-t border-r border-l border-black ${
+                                        forecasts.length - 1 === index
+                                            ? "border-b rounded-bl-md"
+                                            : ""
+                                    }`}
+                                >
+                                    {forecast?.name}
+                                </td>
+                                <td
+                                    className={`py-2 w-[13%] border-t border-r border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {forecast?.state}
+                                </td>
+                                <td
+                                    className={`py-2 w-[11%] border-t border-r border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {forecast?.country}
+                                </td>
+                                <td
+                                    className={`py-2 w-[16%] border-t border-r border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {timeConverter(forecast.forecastForTheDay!.date)}
+                                </td>
+                                <td
+                                    className={`py-2 w-[11%] border-t border-r border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {forecast.forecastForTheDay?.temp}
+                                </td>
+                                <td
+                                    className={`py-2 w-[11%] border-t border-r border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {forecast.forecastForTheDay?.description}
+                                </td>
+                                <td
+                                    className={`py-2 w-[8%] border-t border-r border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {forecast.forecastForTheDay?.main}
+                                </td>
+                                <td
+                                    className={`py-2 w-[9%] border-t border-black ${
+                                        forecasts.length - 1 === index ? "border-b" : ""
+                                    }`}
+                                >
+                                    {forecast.forecastForTheDay?.pressure}
+                                </td>
+                                <td
+                                    className={`py-2 w-[8%] border-t border-r border-l  border-black ${
+                                        forecasts.length - 1 === index
+                                            ? "border-b rounded-br-md"
+                                            : ""
+                                    }`}
+                                >
+                                    {forecast.forecastForTheDay?.humidity}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                    {/* end of data */}
+                </tbody>
             </table>
             <div>
                 <ButtonComp
